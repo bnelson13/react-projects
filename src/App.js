c174ff0fc087459987adc213942266bcd08563cb
Brad Nelson <bnelson13@gmail.com>
2021-01-05 11:26:13 -0800

    Prior to ejecting

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
-
-import ValidationComponent from './ValidationComponent/ValidationComponent';
-import CharComponent from './CharComponent/CharComponent';
+import classes from './App.css';
 
 class App extends Component {
   state = {
-    userInput: '',
-  };
-
-  inputLengthHandler = (e) => {
-    this.setState({ userInput: e.target.value });
-  };
-
-  deleteCharHandler = (index) => {
-    const chars = this.state.userInput.split('');
-    chars.splice(index, 1);
-    const updatedChars = chars.join('');
-    this.setState({ userInput: updatedChars });
+    noState: '',
   };
 
   render() {
-    let inputChars = this.state.userInput.split('').map((char, index) => {
-      return <CharComponent inputChar={char} key={index} click={() => this.deleteCharHandler(index)} />;
-    });
     return (
-      <div className="App">
+      <div className={classes.App}>
         <ol>
-          <li>
-            Create an input field (in App component) with a change listener which outputs the length of the entered text
-            below it (e.g. in a paragraph).
-          </li>
-          <li>Create a new component ({`=>`} ValidationComponent) which receives the text length as a prop</li>
-          <li>
-            Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text
-            length (e.g. take 5 as a minimum length)
-          </li>
-          <li>
-            Create another component ({`=>`} CharComponent) and style it as an inline box ({`=>`} display: inline-block,
-            padding: 16px, text-align: center, margin: 16px, border: 1px solid black).
-          </li>
-          <li>
-            Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in
-            the initial input field) as a prop.
-          </li>
-          <li>When you click a CharComponent, it should be removed from the entered text.</li>
+          <li className={classes.Red}>Test</li>
         </ol>
         <hr />
-        <input type="text" onChange={this.inputLengthHandler} value={this.state.userInput} />
-        <p>{this.state.userInput.length}</p>
-        <ValidationComponent userInput={this.state.userInput.length} />
-        {inputChars}
       </div>
     );
   }
